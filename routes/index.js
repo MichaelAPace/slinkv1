952058b7655a795762f6b6ae12f74ff3e512850b
MichaelAPace <mapace@gmail.com>
2015-07-27 20:57:44 -0600

    commented out extra js

--- routes/index.js
+++ routes/index.js
@@ -10,36 +10,36 @@ router.get('/', function(req, res, next) {
     res.render('index', {docs: docs})
   })
 });
-
-//GET /TEST
-router.get('/test', function (req, res, next){
-  res.render('test')
-})
-
-//GET /TEST 2
-router.get('/test2', function (req, res,next){
-  res.render ('test2')
-})
-
-router.post('/test2', function (req,res, next){
-    console.log(req.body.url)
-    // res.render('test2',{url:req.body.url, name:req.body.name, color:req.body.color});
-    res.render('test2', {body:req.body})
-})
-
-
-
-//TEST 3
-router.get('/test3', function (req, res,next){
-  res.render ('test3')
-})
-
-router.post('/test3', function (req,res, next){
-    console.log(req.body)
-    console.log(req.body)
-    links.insert(req.body, function(err,doc){
-      res.redirect('/', doc)
-    })
-})
+//
+// //GET /TEST
+// router.get('/test', function (req, res, next){
+//   res.render('test')
+// })
+//
+// //GET /TEST 2
+// router.get('/test2', function (req, res,next){
+//   res.render ('test2')
+// })
+//
+// router.post('/test2', function (req,res, next){
+//     console.log(req.body.url)
+//     // res.render('test2',{url:req.body.url, name:req.body.name, color:req.body.color});
+//     res.render('test2', {body:req.body})
+// })
+//
+//
+//
+// //TEST 3
+// router.get('/test3', function (req, res,next){
+//   res.render ('test3')
+// })
+//
+// router.post('/test3', function (req,res, next){
+//     console.log(req.body)
+//     console.log(req.body)
+//     links.insert(req.body, function(err,doc){
+//       res.redirect('/', doc)
+//     })
+// })
 
 module.exports = router;
